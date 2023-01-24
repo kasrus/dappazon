e72d8db2aa20527dfa70e738606991fb11f047ef
Kasey <kruslim@gmail.com>
2023-01-23 16:05:40 -0800

    Added list & buy functionalities into the smart contract. Test file updated

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,76 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint id;
+        string name;
+        string category;
+        string image;
+        uint cost;
+        uint rating;
+        uint stock;
+    }
+     
+    struct Order {
+        uint time;
+        Item item;
+    }
+
+    mapping(uint => Item) public items;
+    mapping(address => uint) public orderCount;
+    mapping(address => mapping(uint => Order)) public orders;
+
+    event List(string name, uint cost, uint quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint _id, 
+        string memory _name, 
+        string memory _category,
+        string memory _image,
+        uint _cost,
+        uint _rating,
+        uint _stock
+    ) public onlyOwner {
+        // Create item struct in the memory
+        Item memory item = Item(_id, _name, _category, _image,
+                                _cost, _rating, _stock);
+
+        // Save Item struct to blockchain -> using mapping
+        items[_id] = item;
+
+        //Emit an event
+        emit List(_name, _cost, _stock);
+    }
+
+    // Buy products
+    function buy(uint _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
+        // Create an order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order for user
+        orderCount[msg.sender]++; //Update the order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Subtract stock
+        item.stock--;
+        
+        // Emit event
+
+    }
+    // Withdraw funds
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,95 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
-describe("Dappazon", () => {
+const ID = 1; 
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
 
+describe('Dappazon', () => {
+  let deployer, buyer;
+  let dappazon;
+
+  beforeEach(async() => {
+    //Setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    //Deploy contract
+    const Dappazon = await ethers.getContractFactory('Dappazon');
+    dappazon = await Dappazon.deploy();
+  }) 
+
+  describe('Deployment', () => {
+    it('sets the owner', async() => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
+
+  describe('Listing', () => {
+    let transaction;
+    
+    beforeEach(async() => {
+      transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE,
+                          COST, RATING, STOCK);  
+      await transaction.wait();
+    })
+
+    describe('Success', () => {
+      it('returns item attributes', async() => {
+        const item = await dappazon.items(ID);
+        expect(item.id).to.equal(ID);
+        expect(item.name).to.equal(NAME);
+        expect(item.category).to.equal(CATEGORY);
+        expect(item.image).to.equal(IMAGE);
+        expect(item.cost).to.equal(COST);
+        expect(item.rating).to.equal(RATING);
+        expect(item.stock).to.equal(STOCK);
+      })    
+  
+      it('emits list event', () => {
+        expect(transaction).to.emit(dappazon);
+      }) 
+  
+      it('sets the owner', async() => { 
+        const result = await dappazon.owner();
+        expect(result).to.equal(deployer.address);
+      }) 
+    })
+
+    describe('Failure', () => {
+      it('rejects anyone than the owner', async() => {
+        await expect(dappazon.connect(buyer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)).to.be.reverted;
+      })
+    })
+  })
+
+  describe('Buying', () => {
+    let transaction;
+    
+    beforeEach(async() => {
+      // List an item
+      transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      // Buy an item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+    })
+
+    it('updates the contract balance', async() => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.equal(COST);
+    })
+
+    it('updates buyer\'s order count', async() => {
+      const result = await dappazon.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    })
+  })
 })
